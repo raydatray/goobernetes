027923a33803d8f20d505bf03e96c606df3b8466
Ray <raydatray@gmail.com>
2025-02-15 18:31:15 -0500

    add max connections per backend

--- pkg/loadbalancer/server.go
+++ pkg/loadbalancer/server.go
@@ -1,9 +1,47 @@
 package loadbalancer
 
 type ServerInstance struct {
+<<<<<<< HEAD
 	ID     string
 	Host   string
 	Port   string
 	Weight int
 	Active bool
+=======
+	ID          string
+	Host        string
+	Port        int
+	Active      bool
+	MaxConns    int
+	connections chan struct{}
+}
+
+func NewServerInstance(id string, host string, port int, maxConns int) *ServerInstance {
+	return &ServerInstance{
+		ID:          id,
+		Host:        host,
+		Port:        port,
+		Active:      true,
+		MaxConns:    maxConns,
+		connections: make(chan struct{}, maxConns),
+	}
+}
+
+func (s *ServerInstance) AcquireConnection() bool {
+	success := false
+	select {
+	case s.connections <- struct{}{}:
+		success = true
+	default:
+		success = false
+	}
+	return success
+}
+
+func (s *ServerInstance) ReleaseConnection() {
+	select {
+	case <-s.connections:
+	default:
+	}
+>>>>>>> 06a8271 (add max connections per backend)
 }
